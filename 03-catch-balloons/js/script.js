ca78ed83c064e605530bb51aeae612234acfa4fc
StephaneLP <stephanelepajolec@outlook.com>
2023-02-28 08:19:43 +0100

    maj

--- 03-catch-balloons/js/script.js
+++ 03-catch-balloons/js/script.js
@@ -1,38 +1,42 @@
-window.addEventListener('load', init)
+// window.addEventListener('load', init)
 
 let stage
 let shapes = []
 let time = 1200
+let goGame = false
+let nombreFigures
 let descriptionfigures = [
     {
-        x: 500, y: 400, diametre: 50, color: "lightblue",
+        x: 520, y: 250, diametre: 100, color: "darkgreen",
+    },
+    {
+        x: 400, y: 250, diametre: 90, color: "darkblue",          
+    },
+    {
+        x: 1000, y: 250, diametre: 80, color: "blue",         
+    },
+     {
+        x: 500, y: 600, diametre: 70, color: "orange",
     },
     {
         x: 800, y: 300, diametre: 60, color: "lightgreen",
     },
     {
-        x: 500, y: 600, diametre: 70, color: "orange",
+        x: 500, y: 400, diametre: 50, color: "lightblue",
     },
     {
-        x: 1000, y: 250, diametre: 80, color: "blue",         
-    },
-    {
-        x: 400, y: 250, diametre: 80, color: "darkblue",          
-    },
-    {
-        x: 520, y: 250, diametre: 100, color: "darkgreen",
+        x: 200, y: 420, diametre: 40, color: "yellow",
     },
     {
        x: 780, y: 550, diametre: 30, color: "green",
     },
-    {
-        x: 200, y: 420, diametre: 150, color: "yellow",
-    }
 ]
 
 function init() {
+    nombreFigures = descriptionfigures.length
     let canvas = document.querySelector('.myCanvas');
     stage = new createjs.Stage(canvas);
+    let tempsRestant
 
     for(let i in descriptionfigures){
         createFigure(descriptionfigures[i])
@@ -40,11 +44,24 @@ function init() {
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF
     createjs.Ticker.addEventListener("tick", () => {
-        time -= 1;
-        if(time==0){
-            alert("Perdu !")
+        if(time%60==0){
+            tempsRestant = time/60
+            document.querySelector("#timer").textContent = tempsRestant.toString() + "s"
         }
-        animate();
+        if(goGame){
+            time -= 1;
+            if(nombreFigures==0){
+                document.getElementById("resultat").style.display = "block";
+                goGame = false
+            }
+            else if(time==0){
+                document.getElementById("resultat").innerText = "Perdu !"
+                document.getElementById("resultat").style.display = "block";
+                goGame = false
+            }
+            animate();
+        }
+
         stage.update();
     })
 }
@@ -57,9 +74,10 @@ function createFigure(descriptionfigures) {
     figure.diametre = descriptionfigures.diametre;
     figure.color = descriptionfigures.color;
 
-    figure.animation = true;
+    figure.animation = false;
     figure.direction = getRandomIntInclusive(1,4);
-    figure.valdir = -3;
+    figure.valdir = 3;
+    figure.clique = false;
 
     figure.graphics.beginFill(figure.color).drawCircle(0, 0, figure.diametre);
     figure.description = "Disque de couleur " + figure.color;
@@ -68,7 +86,8 @@ function createFigure(descriptionfigures) {
     stage.addChild(figure);
 
     figure.addEventListener("click", (event) => {
-        event.currentTarget.animation = !event.currentTarget.animation
+        nombreFigures -= 1
+        event.currentTarget.clique = true
     })
 }
 
@@ -80,25 +99,34 @@ function animate(){
             posx = shapes[i].x - shapes[i].diametre
             posy = shapes[i].y - shapes[i].diametre
             val = shapes[i].diametre*2
-            changeDirection = ((posx < 0)||(posx > 1200 - val)||(posy < 0)||(posy > 800 - val))
+            changeDirection = ((posx < 0)||(posx > 1200 - val)||(posy < 0)||(posy > 700 - val))
 
             switch(shapes[i].direction){
                 case 1:
-                    if(changeDirection){
+                    if(shapes[i].clique){
+                        shapes[i].valdir += 10
+                    }
+                    else if(changeDirection){
                         shapes[i].valdir = shapes[i].valdir*(-1)
                         shapes[i].direction = getRandomIntInclusive(1,4)
                     }
                     shapes[i].y += shapes[i].valdir;
                     break;
                 case 2:
-                   if(changeDirection){
+                    if(shapes[i].clique){
+                        shapes[i].valdir += 10
+                    }
+                    else if(changeDirection){
                         shapes[i].valdir = shapes[i].valdir*(-1)
                         shapes[i].direction = getRandomIntInclusive(1,4)
                     }
                     shapes[i].x += shapes[i].valdir;
                     break;
                 case 3:
-                    if(changeDirection){
+                    if(shapes[i].clique){
+                        shapes[i].valdir += 10
+                    }
+                    else if(changeDirection){
                         shapes[i].valdir = shapes[i].valdir*(-1)
                         shapes[i].direction = getRandomIntInclusive(1,4)
                     }
@@ -106,15 +134,34 @@ function animate(){
                     shapes[i].y += shapes[i].valdir;
                     break;
                 case 4:
-                    if(changeDirection){
+                    if(shapes[i].clique){
+                        shapes[i].valdir += 10
+                    }
+                    else if(changeDirection){
                         shapes[i].valdir = shapes[i].valdir*(-1)
                         shapes[i].direction = getRandomIntInclusive(1,4)
                     }
                     shapes[i].x += shapes[i].valdir;
                     shapes[i].y += shapes[i].valdir*(-1);
                     break;
-            }
+            }                
         }
+    }
+}
+
+function go(){
+    init()
+    if(!goGame){
+        for(let i in shapes){
+            shapes[i].animation = true;
+        } 
+        goGame = true      
+    }
+    else{
+        for(let i in shapes){
+            shapes[i].animation = false;
+        }  
+        document.getElementById("resultat").style.display = "none";
     }
 }
 
