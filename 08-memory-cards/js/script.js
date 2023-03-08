c583f49269d01e5186b04ee8ef9d96c0c1b718ee
StephaneLP <stephanelepajolec@outlook.com>
2023-03-08 22:02:29 +0100

    maj

--- 08-memory-cards/js/script.js
+++ 08-memory-cards/js/script.js
@@ -4,27 +4,56 @@ let tabCases = document.querySelectorAll(".card")
 let tabImages = ["alarmclock","baloon","box","butterfly","hat","paperplane","poolball","radio","rocket","rubikscube","television","tiebow"]
 
 function main(){
-    initCases()    
+    initCards()
+    initListeners()
 }
 
-function initCases(){
-    //chercher 6 indices dans tabImages
+/****************************************************************************************
+INITIALISATION DES CARTES
+****************************************************************************************/
 
-
-
+function initCards(){
     let tabTirageImages = randomArray(6,tabImages)
     let tabCards = [0,1,2,3,4,5,6,7,8,9,10,11]
     tabCards = randomArray(tabCards.length,tabCards)
     let tabPairesCards = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 
+    for (let i=0; i<tabCases.length; i++){
+        tabCases[i].children[0].children[1].style.backgroundImage = "url('img/question.svg')"
+    }
+
     for(let i in tabTirageImages){
-        tabCases[tabCards[i*2]].children[0].children[1].style.backgroundImage = "url('img/cards/" + tabTirageImages[i] + ".svg')"
-        tabCases[tabCards[i*2+1]].children[0].children[1].style.backgroundImage = "url('img/cards/" + tabTirageImages[i] + ".svg')"
+        tabCases[tabCards[i*2]].children[0].children[0].style.backgroundImage = "url('img/cards/" + tabTirageImages[i] + ".svg')"
+        tabCases[tabCards[i*2+1]].children[0].children[0].style.backgroundImage = "url('img/cards/" + tabTirageImages[i] + ".svg')"
         tabPairesCards[i][0] = tabCards[i*2]
         tabPairesCards[i][1] = tabCards[i*2+1]
     }
-    // console.log(tabPairesCards)
 }
+
+/****************************************************************************************
+INITIALISATION DES LISTENERS (EVENEMENT CLICK SUR LES CARTES)
+****************************************************************************************/
+
+function initListeners(){
+    for(let i=0; i<tabCases.length; i++){
+        tabCases[i].addEventListener("click",(e)=>{
+            clickCard(e)
+        })
+    }
+}
+
+/****************************************************************************************
+CLICK : ACTION SUR LES CARTES
+****************************************************************************************/
+
+function clickCard(e){
+    e.currentTarget.children[0].classList.add("active");
+
+}
+
+/****************************************************************************************
+OUTILS
+****************************************************************************************/
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
@@ -43,42 +72,4 @@ function randomArray(n,array){
         tab.splice(indice,1)
     }
     return tabRes
-}
-
-
-
-
-
-
-
-
-
-
-let tabLignesGagnantes = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-let nbCliques = 0
-
-
-
-function verification() {
-    let ligneGagnante
-
-    for(let i in tabLignesGagnantes){
-        ligneGagnante = ""
-
-        for(let j in tabLignesGagnantes[i]){
-            ligneGagnante += tabCases[tabLignesGagnantes[i][j]].textContent
-        }
-        if(ligneGagnante === "XXX"){
-            afficheLigneGagnante(i)
-        }
-        if(ligneGagnante === "OOO"){
-            afficheLigneGagnante(i)
-        }        
-    }
-}
-
-function afficheLigneGagnante(i){
-    for(let j in tabLignesGagnantes[i]){
-        tabCases[tabLignesGagnantes[i][j]].style.backgroundColor = "green"
-    }
 }
